1ebb64f65c59e69586d761b0470d411bfe7126a7
limuyu <1003152643@qq.com>
2022-11-04 03:01:36 +0800

    博客管理系统 v1.34 swagger支持token，优化了部分代码

--- myblog/errmsg/errmsg.go
+++ myblog/errmsg/errmsg.go
@@ -33,10 +33,10 @@ var codeMsg = map[int]string{
 	ERROR_USERNAME_USED:    "用户名已存在",
 	ERROR_PASSWORD_WRONG:   "密码错误",
 	ERROR_USER_NOT_EXIST:   "用户不存在",
-	ERROR_TOKEN_EXIST:      "TOKEN不存在，请重新登陆",
-	ERROR_TOKEN_RUNTIME:    "TOKEN已过期，请重新登陆",
-	ERROR_TOKEN_WRONG:      "TOKEN不正确，请重新登陆",
-	ERROR_TOKEN_TYPE_WRONG: "TOKEN格式错误，请重新登录",
+	ERROR_TOKEN_EXIST:      "TOKEN不存在,请重新登陆",
+	ERROR_TOKEN_RUNTIME:    "TOKEN已过期,请重新登陆",
+	ERROR_TOKEN_WRONG:      "TOKEN不正确,请重新登陆",
+	ERROR_TOKEN_TYPE_WRONG: "TOKEN格式错误,请重新登录",
 	ERROR_USER_NO_RIGHT:    "该用户无权限",
 
 	//code=2000...文章模块的错误
